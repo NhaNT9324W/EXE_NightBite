826ecb4e0239780e6e4e631276be460fc4ed202b
NhaNT924W <nhantce181298@fpt.edu.vn>
2026-06-13 10:13:09 +0700

    [SETUP] Initialize backend and frontend workspace

---
+++ nightbite-backend/src/main/java/com/nightbite/NightbiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nightbite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightbiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightbiteBackendApplication.class, args);
+	}
+
+}
---
+++ nightbite-backend/src/test/java/com/nightbite/NightbiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nightbite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightbiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
